81e05537f7659ed9f82900b83a6b85ab9a4c805b
Amishee28 <104733643+Amishee28@users.noreply.github.com>
2022-05-05 21:34:52 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1 =  int(input("enter first number:  "))
+num2 =  int(input("enter second number: "))
+op = input("select operation ['+','-','/','*']:")
+
+if op == '+':
+    print(num1+num2)
+elif op == '-':
+    print(num1-num2)
+elif op == '*':
+    print(num1*num2)
+elif op == '/':
+    print(num1/num2)
+else:
+    print*("invalid operation")
+    
+
